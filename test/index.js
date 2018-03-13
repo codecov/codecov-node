fbf17d2d607feccc3007020b53218c77f0b16932
dotnetCarpenter <jon.ronnenberg@gmail.com>
2018-03-13 21:15:00 +0100

    remove timer because slow CI servers need more than half a second before data is flowing - switch to use --pipe or -l as signal to codecov to recieve data via stdin

--- test/index.js
+++ test/index.js
@@ -38,8 +38,8 @@ describe("Codecov", function(){
   });
 
   it("can specify report in cli", function(){
-    var res = codecov.upload({options: {dump: true, file: 'test' + pathSeparator + 'coverage.json'}});
-    expect(res.files[0].split(pathSeparator).pop()).to.eql('coverage.json');
+    var res = codecov.upload({options: {dump: true, file: 'test' + pathSeparator + 'example.coverage.txt'}});
+    expect(res.files[0].split(pathSeparator).pop()).to.eql('example.coverage.txt');
     expect(res.body).to.contain('this file is intentionally left blank');
   });
 
@@ -134,7 +134,7 @@ describe("Codecov", function(){
 
   it("can read piped reports", function(done){
     var exec = require('child_process').exec;
-    var childProcess = exec('cat test/coverage.json | bin/codecov --dump --disable=gcov', function(err, stdout, stderr){
+    var childProcess = exec('cat test/example.coverage.txt | bin/codecov -l --dump --disable=gcov', function(err, stdout, stderr){
       expect(stdout.toString()).to.contain('path=piped');
       expect(stdout.toString()).to.contain('this file is intentionally left blank');
       childProcess.kill();
