b1eb25f373fc7e4a53495321c71ef7d10ccfd0ef
Christian A. Jacobsen <christian@jenkins.no>
2019-06-06 08:12:18 +0200

    Adds the build tag back
    As requested, the `build` tag has been added back to the config and will
    now be read from the `SEMAPHORE_WORKFLOW_ID` as it's unique per 'run
    through the CI pipeline'.

--- test/services/semaphore.test.js
+++ test/services/semaphore.test.js
@@ -7,12 +7,14 @@ describe('Semaphore CI Provider', function() {
   })
 
   it('can get semaphore env info', function() {
-    process.env.SEMAPHORE_GIT_SHA = '5c84719708b9b649b9ef3b56af214f38cee6acde'
     process.env.SEMAPHORE_GIT_BRANCH = 'development'
+    process.env.SEMAPHORE_GIT_SHA = '5c84719708b9b649b9ef3b56af214f38cee6acde'
+    process.env.SEMAPHORE_WORKFLOW_ID = '65c9bb1c-aeb6-41f0-b8d9-6fa177241cdf'
     expect(semaphore.configuration()).toEqual({
       service: 'semaphore',
-      commit: '5c84719708b9b649b9ef3b56af214f38cee6acde',
       branch: 'development',
+      build: '65c9bb1c-aeb6-41f0-b8d9-6fa177241cdf',
+      commit: '5c84719708b9b649b9ef3b56af214f38cee6acde',
     })
   })
 })
